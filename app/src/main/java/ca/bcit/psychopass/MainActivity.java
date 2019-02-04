6bb44a04155029de24af2060dda2867408a44791
jjia <jjia9@my.bcit.ca>
2019-02-04 14:00:27 -0800

    First Commit

--- app/src/main/java/ca/bcit/psychopass/MainActivity.java
+++ app/src/main/java/ca/bcit/psychopass/MainActivity.java
@@ -2,6 +2,9 @@ package ca.bcit.psychopass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        WebView webview = (WebView) findViewById(R.id.webview1);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://www.google.com/maps/place/Maple+Ridge,+BC/@49.2599033,-122.6800957,11z/data=!3m1!4b1!4m5!3m4!1s0x5485d3614f013ecb:0x47a5c3ea30cde8ea!8m2!3d49.2193226!4d-122.5983981");
     }
+
 }
