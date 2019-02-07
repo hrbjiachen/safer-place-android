6f5f97fe54c984773b57e6ed4fe9c5aa04920705
Hysteresis <crystalloid1@gmail.com>
2019-02-06 22:46:07 -0800

    wrap gps

--- app/src/main/java/ca/bcit/psychopass/MainActivity.java
+++ app/src/main/java/ca/bcit/psychopass/MainActivity.java
@@ -8,24 +8,24 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements myLocationUtil.myLocationCallback {
 
     private WebView webView;
 
     public static final String GOOGLE_MAP_URL = "https://www.google.com/maps/";
+
+    private boolean locationRegistered = false;
 
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity  {
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        setupGPS();
+        locationRegistered =  myLocationUtil.registerCallback(this, this);
 
         webView = findViewById(R.id.webview1);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -44,53 +44,18 @@ public class MainActivity extends AppCompatActivity  {
         webView.loadUrl("https://www.google.com/maps/place/Maple+Ridge,+BC/@49.2599033,-122.6800957,11z/data=!3m1!4b1!4m5!3m4!1s0x5485d3614f013ecb:0x47a5c3ea30cde8ea!8m2!3d49.2193226!4d-122.5983981");
     }
 
-    private void setupGPS(){
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        1);
-
-                // MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        String provider = locationManager.getBestProvider(criteria, true);
-        locationManager.requestLocationUpdates(provider, 5, 30, new LocationListener() {
-            public void onLocationChanged(Location location) {
-                // Called when a new location is found by the network location provider.
-                double longitude = location.getLongitude();
-                double latitude = location.getLatitude();
-                Toast.makeText(MainActivity.this, longitude + " " + latitude, Toast.LENGTH_SHORT).show();
-                String url = GOOGLE_MAP_URL + "@" + latitude + "," + longitude;
-                Log.d("googlemap", url);
-                webView.loadUrl(url);
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-            public void onProviderEnabled(String provider) {}
-
-            public void onProviderDisabled(String provider) {}
-        });
+    @Override
+    public void onLocationChange(Location location) {
+        double la = location.getLatitude();
+        double lon = location.getLongitude();
+        Toast.makeText(this, la + ":" + lon, Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!locationRegistered){
+            locationRegistered = myLocationUtil.registerCallback(this, this);
+        }
+    }
 }
