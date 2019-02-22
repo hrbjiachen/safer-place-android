d605d6a77e2dcdaac9a4c5c0ec47ff57379e7eab
Jason Jia <jjia9@my.bcit.ca>
2019-02-22 12:59:13 -0800

    update toolbar

--- app/src/main/java/ca/bcit/psychopass/MainActivity.java
+++ app/src/main/java/ca/bcit/psychopass/MainActivity.java
@@ -15,7 +15,10 @@ import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -56,10 +59,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         locationServiceCheck();
         setInitialWebView();
-        initializeSettingIcon();
-        initializeLocationIcon();
+//        initializeSettingIcon();
+//        initializeLocationIcon();
 
         MyJsonUtil jsonUtil = new MyJsonUtil(MainActivity.this,getApplicationContext());
         jsonUtil.parseLocalJSON();
@@ -89,36 +95,36 @@ public class MainActivity extends AppCompatActivity {
         webView.loadUrl(INITIAL_LOCATION);
     }
 
-    public void initializeLocationIcon() {
-        ImageView location = findViewById(R.id.locationImg);
-        location.setClickable(true);
-        location.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(curLocation != null){
-                    DecimalFormat df = new DecimalFormat("#.#######");
-                    double Lat = Double.valueOf(df.format(curLocation.getLatitude()));
-                    double Long = Double.valueOf(df.format(curLocation.getLongitude()));
-                    Log.e(TAG, Lat + ", " + Long);
-
-                    webView = findViewById(R.id.mapWebView);
-                    webView.loadUrl(GOOGLE_MAP_URL + Lat + "," + Long + ",16z");
-                }
-            }
-        });
-    }
-
-    public void initializeSettingIcon() {
-        ImageView setting = findViewById(R.id.settingImg);
-        setting.setClickable(true);
-        setting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+//    public void initializeLocationIcon() {
+//        ImageView location = findViewById(R.id.locationImg);
+//        location.setClickable(true);
+//        location.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(curLocation != null){
+//                    DecimalFormat df = new DecimalFormat("#.#######");
+//                    double Lat = Double.valueOf(df.format(curLocation.getLatitude()));
+//                    double Long = Double.valueOf(df.format(curLocation.getLongitude()));
+//                    Log.e(TAG, Lat + ", " + Long);
+//
+//                    webView = findViewById(R.id.mapWebView);
+//                    webView.loadUrl(GOOGLE_MAP_URL + Lat + "," + Long + ",16z");
+//                }
+//            }
+//        });
+//    }
+//
+//    public void initializeSettingIcon() {
+//        ImageView setting = findViewById(R.id.settingImg);
+//        setting.setClickable(true);
+//        setting.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
 
     public void onClickBtn(View v) {
         //testing coordinates
@@ -178,6 +184,34 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu. This adds items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.action_location:
+                DecimalFormat df = new DecimalFormat("#.#######");
+                double Lat = Double.valueOf(df.format(curLocation.getLatitude()));
+                double Long = Double.valueOf(df.format(curLocation.getLongitude()));
+                Log.e(TAG, Lat + ", " + Long);
+
+                webView = findViewById(R.id.mapWebView);
+                webView.loadUrl(GOOGLE_MAP_URL + Lat + "," + Long + ",16z");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
